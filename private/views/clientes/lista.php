6b86dd6fb436ecc27536865cd3e78098f349ff4c
Matilde Rocha <1240962@isep.ipp.pt>
2026-06-11 20:05:02 +0100

    Consulta dos dados do cliente

--- private/views/clientes/lista.php
+++ private/views/clientes/lista.php
@@ -108,8 +108,7 @@ $ligacao = null;
                                 <td><?= $cliente->telefone ?></td>
                                 <td><?= $cliente->morada . ' - ' . $cliente->cidade ?></td>
                                 <td class="text-center">
-                                    <a href="detalhes.php" class="btn btn-sm btn-outline-primary me-1"> <i
-                                            class="fa-solid fa-eye"></i>
+                                    <a href="detalhes.php?id_cliente=<?= aes_encrypt($cliente->id) ?>" class="btn btn-sm btn-outline-primary me-1"> <i class="fa-solid fa-eye"></i>
                                     </a>
                                     <a href="editar.php?id_cliente=<?= aes_encrypt($cliente->id) ?>" class="btn btn-sm btn-outline-warning me-1"> <i
                                             class="fa-regular fa-pen-to-square"></i>
